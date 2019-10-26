14db11b8520aa2754966c2883432e243f005771c
[Shashank-Venkatraman].github.io <48584298+Shashank-Venkatraman@users.noreply.github.com>
2019-10-26 10:20:09 +0530

    Add files via upload

---
+++ for-grids.js
@@ -0,0 +1,8 @@
+ $( function() {
+	 	 "use strict";
+
+    $( ".widget input[type=submit], .widget a, .widget button" ).button();
+    $( "button, input, a" ).click( function( event ) {
+      event.preventDefault();
+    } );
+  } );
